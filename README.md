562b916bf432cf20103c2ccdf0205027085793bf
yexudong <ye850570138@sina.com>
2016-02-02 00:37:08 +0800

    test2

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 FloatingActionButton
           
-app:backgroundTint="#232323": //设置背景色       
+app:backgroundTint="#232323": //设置背景色   
+    
+app:fabSize="";//可选值为mini何normal(默认)
+
+app:evevation="2dp";//设置阴影
+
+app:rippleColor="";//设置点击按钮时的颜色
+
+TextInputLayout(让EditText更加人性化)
+
+里面必须放一个ET
+
+![](mipmap-hdpi/c1.png)
--- app/src/main/java/com/example/administrator/helloas/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloas/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.administrator.helloas;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,12 +10,18 @@ public class MainActivity extends AppCompatActivity {
     //浮动的圆形按钮
     private FloatingActionButton fab;
 
+    private TextInputLayout til;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        til = (TextInputLayout) findViewById(R.id.textInput);
+
+        til.setHint("请输入用户名");
 
     }
 }
